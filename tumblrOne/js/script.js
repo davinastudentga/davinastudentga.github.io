895f30aade5386dc1604233c4ccce823fa878ff7
davinastudentga <davinastudentga@gmail.com>
2015-03-28 20:30:02 -0400

    update

--- tumblrOne/js/script.js
+++ tumblrOne/js/script.js
@@ -1,13 +1,15 @@
 var main = function(){
-	var state = true; // toggle effect for side left menu by clicking on the hamburger button
+	var state = true; // toggle effect for off-canvas left menu by clicking on the hamburger button
 	$('.hamburgerToggleButton').click(function() {
 		if (state) {
 			$('.sideMenu').animate({left: "0px"}, 200);
 			$('body').animate({left: "300px"}, 200);
+			$('body').css("overflow", "hidden");
 		}
 		else {
 			$('.sideMenu').animate({left: "-300px"}, 200);
 			$('body').animate({left: "0px"}, 200);
+			$('body').css("overflow", "auto");
 		}
 		state = !state;
 	});
